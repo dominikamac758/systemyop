11cd2287e4b01de68723bb5b3ef30e42a4e6044b
dominikamac758 <dominika06maciejewska@gmail.com>
2025-12-03 09:14:51 +0100

    Update zadania_watki_opis.sh

--- zadania_watki_opis.sh
+++ zadania_watki_opis.sh
@@ -20,3 +20,21 @@ pthread_join, który gwarantuje, że czekamy na wszystkie wątki,
 oraz zebranie wyników z poszczególnych wątków.
 
 Całość pokazuje, jak można przyspieszyć obliczenia dzięki równoległości.
+
+
+
+W tym programie sprawdzam, jak zmienia się czas wykonywania sumowania dużej tablicy liczb w zależności od trzech parametrów:
+n – ile elementów ma jedna podtablica,
+k – ile takich podtablic tworzymy,
+t – ile używamy wątków.
+
+Dla każdej kombinacji tych wartości program generuje tablicę liczb o rozmiarze n razy k. Następnie dzieli tę tablicę na fragmenty i rozdziela je między wątki. Każdy wątek sumuje swój własny fragment, czyli działa równolegle z innymi.
+
+Najważniejszym elementem tego programu jest pomiar czasu wykonania operacji wielowątkowego sumowania.
+Do pomiaru używam funkcji gettimeofday(), która zwraca czas w mikrosekundach, czyli milionowych częściach sekundy. Dzięki temu mogę bardzo dokładnie zmierzyć, ile trwa wykonanie danego zestawu obliczeń.
+
+Czas startowy zapisuję tuż przed uruchomieniem wszystkich wątków, a czas końcowy tuż po zakończeniu pracy ostatniego wątku. Różnica między tymi dwoma wartościami to rzeczywisty czas wykonania wielowątkowego sumowania.
+
+Program powtarza ten pomiar dla różnych wartości n, k i t, a wyniki zapisuje do pliku CSV, dzięki czemu można później narysować wykres lub przeanalizować, jak liczba wątków wpływa na szybkość działania programu.
+
+Dzięki temu program pozwala ocenić, kiedy wielowątkowość przyspiesza obliczenia, a kiedy liczba wątków jest już zbyt duża i nie daje zysku.”
